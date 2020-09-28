de36dd87e9a14d15142e264266045fc37dbdbf7e
h3rb <hgilliland@gmail.com>
2020-09-28 10:32:28 -0400

    Missing file

---
+++ framework/include/cli_help.php
@@ -0,0 +1,39 @@
+/******************************************************************
+ __     __    _     _       __    _      _   __
+/ /`_  / /\  | |   \ \  /  / /\  | |\ | | | / /`
+\_\_/ /_/--\ |_|__  \_\/  /_/--\ |_| \| |_| \_\_,
+
+ Copyright (c) 2020 H. Elwood Gilliland III
+ This open source framework is available under the MIT License,
+ see LICENSE file accompanying this file for more information
+*******************************************************************/
+#pragma once
+
+#include "zerotypes.h"
+
+#include "commandline.h"
+
+
+class CLI_Help : public CommandLineFeature {
+public:
+ CLI_Help() : CommandLineFeature() {
+  key="help";
+  shortkey="h";
+  help="Show command line options.";
+  text="galvanic --request=[filepath.json]";
+ }
+ string Fit(const char *in) {
+  Zsstring s(in);
+  while ( s.length < 25 ) s+=" ";
+  return s.value;
+ }
+ Zstring text;
+ Strings addendums;
+ void Action( CommandLineOption *in ) {
+  Zstring output=text;
+  EACH(commandLineFeatures.first,CommandLineFeature,clf) { output+=Fit(clf->key.c_str()); output+=clf->help; output+="\n"; }
+  EACH(addendums.first,String,s) { output+=s->c_str(); output+="\n"; }
+  cout << output.value << endl;
+ }
+
+};
---
+++ framework/include/new_file_template.h
@@ -0,0 +1,12 @@
+/******************************************************************
+ __     __    _     _       __    _      _   __
+/ /`_  / /\  | |   \ \  /  / /\  | |\ | | | / /`
+\_\_/ /_/--\ |_|__  \_\/  /_/--\ |_| \| |_| \_\_,
+
+ Copyright (c) 2020 H. Elwood Gilliland III
+ This open source framework is available under the MIT License,
+ see LICENSE file accompanying this file for more information
+*******************************************************************/
+#pragma once
+
+#include "zerotypes.h"
