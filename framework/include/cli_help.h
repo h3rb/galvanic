37f13ef733bf8af10485434a28512fec9ef7a15a
h3rb <hgilliland@gmail.com>
2020-10-21 21:09:52 -0400

    CLI Help

--- framework/include/cli_help.h
+++ framework/include/cli_help.h
@@ -33,9 +33,9 @@ public:
   Zstring output=text;
   EACH(commandLineFeatures.first,CommandLineFeature,clf) {
    Zstring equalstext;
-   if ( clf->varname.length > 0 ) equalstext=string("=[")+clf->varname+string("]    ");
+   if ( clf->varname.length > 0 ) equalstext=string("=[")+clf->varname+string("]");
    if ( clf->shortkey.length > 0 && clf->key.length > 0 ) {
-    output+=Fit( (string("-")+clf->shortkey+equalstext+string(", ")+clf->key+equalstext).c_str());
+    output+=Fit( (string("-")+clf->shortkey+equalstext+string(", ")+clf->key+equalstext+string("  ")).c_str());
    } else if ( clf->shortkey.length > 0 )
     output+=Fit((string("-")+clf->shortkey).c_str());
    else if ( clf->key.length > 0 ) {
