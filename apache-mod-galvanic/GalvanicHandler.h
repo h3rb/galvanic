926dad2964a4febd495c6a935760143de7011d1a
RetroFriends <lostastronaut@rocketship.com>
2020-12-07 13:10:17 -0500

    Postings..

--- apache-mod-galvanic/GalvanicHandler.h
+++ apache-mod-galvanic/GalvanicHandler.h
@@ -13,6 +13,9 @@ class GalvanicHandler {
         int RunHandler(request_rec *req) {
             if (!req->handler || module_name != req->handler) return DECLINED;
             InitZeroTypesLibrary();
+            GetHeaders(req);
+            GetPost(req);
+            GetBody(req);
             Galvanic galvanic;
             http_out=galvanic.Execute( HEADERS, POST, body );
             ap_rputs(http_out.c_str(), req);              
@@ -22,6 +25,34 @@ class GalvanicHandler {
         string module_name = "galvanic";
         Strings HEADERS, POST;
         Zstring body, http_out;
+        void GetHeaders(request_rec *req) {
+            
+        }
+        void GetPost(request_rec *req) {
+         apr_array_header_t *pairs = NULL;
+         apr_off_t len;
+         apr_size_t size;
+         int res;
+         int i = 0;
+         
+         res = ap_parse_form_data(r, NULL, &pairs, -1, HUGE_STRING_LEN);
+         if (res != OK || !pairs) return NULL; /* Return NULL if we failed or if there are is no POST data */
+         kvp = apr_pcalloc(r->pool, sizeof(keyValuePair) * (pairs->nelts + 1));
+         while (pairs && !apr_is_empty_array(pairs)) {
+             ap_form_pair_t *pair = (ap_form_pair_t *) apr_array_pop(pairs);
+             apr_brigade_length(pair->value, 1, &len);
+             char buff[len+1];
+             buff[len]=0;
+             apr_brigade_flatten(pair->value, buff, &size);
+             String *s=new String;
+             s->s=string(pair->name);
+             s->value = string(buff);
+             i++;
+         }
+        }
+        void GetBody(request_rec *req) {
+            
+        }
 };
 
 extern GalvanicHandler galvanicHandler;
