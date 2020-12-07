2bd9f1d06dc2781fb6df7bab21e4a683f052835f
RetroFriends <lostastronaut@rocketship.com>
2020-12-07 10:53:06 -0500

    Switching

--- apache-mod-galvanic/GalvanicHandler.h
+++ apache-mod-galvanic/GalvanicHandler.h
@@ -3,12 +3,23 @@
 #include <string>
 using std::string;
 
-struct request_rec;
 #include "../framework/include/zerotypes.h"
+#include "http_protocol.h"
 
 class GalvanicHandler {
     public:
-        int RunHandler(request_rec *req);
+        int RunHandler(request_rec *req) {
+            if (!req->handler || module_name != req->handler) return DECLINED;
+
+            int main(int argc, char **argv ) {
+            InitZeroTypesLibrary();
+            Galvanic galvanic;
+            int result=galvanic.ModuleHandler( req );
+            if ( result ) {
+              ap_rputs(http_out.c_str(), req);              
+            }
+            return OK;
+        }
     private:
         string module_name = "galvanic";
         Zstring http_out;
